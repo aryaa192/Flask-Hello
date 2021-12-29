866579df9e328add1f292f77bccc17ace2e2a9a4
aryaa192 <amitarya192@gmail.com>
2021-12-29 19:23:16 +0530

    Flask Hello App

---
+++ helloapp/routes.py
@@ -0,0 +1,36 @@
+from helloapp import app, render_template,redirect,url_for
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html', title='Welcome to the homepage.', user='Aniket')
+
+@app.route("/user/<username>/")
+def hello_user(username):
+    return render_template('index.html', title='Welcome to the homepage.', user=username)
+
+@app.route("/user/<username>/<int:age>/")
+
+def display_age(username, age):
+
+    return "Hello " + username +"!!!<br>You are " + str(age) + " years old."
+
+@app.route("/home/")
+
+def demo_redirect():
+
+    return redirect("http://localhost:5000/")
+
+@app.route("/greet/user/<uname>")
+
+def greet_user(uname):
+
+   return redirect(url_for('hello_user', username=uname))
+
+@app.route("/users/")
+
+def display_users():
+
+    users = ['John', 'Rosy', 'Jack', 'Sammy', 'Lilly']
+
+    return render_template('users.html', title='Users', users=users)
